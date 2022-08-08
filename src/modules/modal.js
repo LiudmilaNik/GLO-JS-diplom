27a42fcd1d482382d3423c3dc57b143cadae3cd7
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-08-08 23:49:16 +0300

    Add files via upload

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,8 +1,40 @@
 " use strict";
-const modal = () => {
-  const modal = "my modal";
 
-  console.log(modal);
+const modal = () => {
+  const callbackBtn = document.querySelectorAll(".callback-btn");
+  const servicesElements = document.querySelector(".services-elements");
+  const buttonServises = document.querySelector(".button-services");
+  const modal = document.querySelector(".modal-callback");
+  const modalOverlay = document.querySelector(".modal-overlay");
+
+  const openModal = (e) => {
+    e.preventDefault();
+    modal.style.display = "block";
+    modalOverlay.style.display = "block";
+    document.body.style.overflow = "hidden";
+  };
+
+  callbackBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => openModal(e));
+  });
+
+  servicesElements.addEventListener("click", (e) => {
+    if (e.target.closest(".absolute")) {
+      openModal(e);
+    }
+  });
+
+  buttonServises.addEventListener("click", (e) => openModal(e));
+
+  modalOverlay.addEventListener("click", (e) => {
+    if (
+      e.target.closest(".modal-overlay") ||
+      e.target.classList.contains("modal-close")
+    ) {
+      modalOverlay.style.display = "none";
+      document.body.style.overflow = "";
+    }
+  });
 };
 
 export default modal;

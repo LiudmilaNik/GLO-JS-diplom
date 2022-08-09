a935b86687269d8b9c310f02c64bb70f51f072fd
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-08-09 18:43:24 +0300

    Update sendForm.js

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -68,22 +68,6 @@ const sendForm = (someElem = []) => {
     }
   });
 
-  // form.addEventListener("submit", (e) => {
-  //   e.preventDefault();
-
-  //   const formData = new FormData(form);
-  //   const formBody = {};
-
-  //   formData.forEach((val, key) => {
-  //     formBody[key] = val;
-  //   });
-
-  //   console.log("Submit");
-
-  //   sendData(formBody).then((data) => {
-  //     console.log(data);
-  //   });
-  // });
-};
+ };
 
 export default sendForm;

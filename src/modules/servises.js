346005b5f13dae42741c769d584260bff6c78e73
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-08-12 22:31:33 +0300

    Add files via upload

--- src/modules/servises.js
+++ src/modules/servises.js
@@ -4,6 +4,21 @@ const servises = () => {
     slidesPerView: 3,
     loop: true,
 
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+    },
     navigation: {
       nextEl: ".arrow-right",
       prevEl: ".arrow-left",

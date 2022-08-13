053bb1d1ebd45b8fc135bc40d4cf738a23ebf153
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-08-13 11:59:16 +0300

    Add files via upload

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,23 +1,5 @@
 " use strict";
 const accordeon = () => {
-  // const accordeon = document.querySelector(".accordeon");
-  // const elements = accordeon.querySelectorAll(".element");
-  // const answer = document.querySelectorAll(".element-content");
-
-  // accordeon.addEventListener("click", (e) => {
-  //   const target = e.target.closest(".element");
-  //   if (target) {
-  //     elements.forEach((item, i) => {
-  //       if (item === target) {
-  //         elements[i].classList.add("active");
-  //         answer[i].classList.add("accordeon-active");
-  //       } else {
-  //         elements[i].classList.remove("active");
-  //         answer[i].classList.remove("accordeon-active");
-  //       }
-  //     });
-  //   }
-  // });
   const accordeon = document.querySelector(".accordeon");
   const elements = accordeon.querySelectorAll(".element");
 
